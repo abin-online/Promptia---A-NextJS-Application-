fec62d52325079428aef99d998ed5ead80feede0
Abin Babu <abinbabuonline@gmail.com>
2024-11-05 11:09:36 +0530

    prompt models

---
+++ models/prompt.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const PromptSchema = new Schema({
+    creator: {
+        type: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    },
+
+    prompt: {
+        type: String,
+        required: [true, 'Prompt is required']
+    },
+
+    tag: {
+        type: String,
+        required: [true, 'Tag is requred']
+    },
+
+})
+
+const Prompt = models.Prompt || model('Prompt', PromptSchema)
+
+export default Prompt
